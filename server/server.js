92f86dcc778cade1e50f74e80100811d87a29fc0
caroline <cluce059@gmail.com>
2021-11-09 21:47:14 -0500

    jic

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const {typeDefs, resolvers} = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -6,6 +10,14 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//set up server
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware 
+});
+
+server.applyMiddleware({ app});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +26,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
